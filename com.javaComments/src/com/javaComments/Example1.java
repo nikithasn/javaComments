d815ad4570aca357a048d4896de7fd8ed66686b2
nikitha <nikitha@LAPTOP-4GAKUODQ>
2022-02-04 10:25:46 +0530

    first commit

---
+++ com.javaComments/src/com/javaComments/Example1.java
@@ -0,0 +1,29 @@
+//user defined package name=> it is single line comment
+package com.javaComments;
+
+
+/*
+ * user define class name,
+ * 
+ * we can declare user define class name with class keyword
+ * 
+ * Example1 is user defined class name.
+ */
+public class Example1 
+{
+	
+	/**
+	 * 
+	 * main method: every java program or application execution starts from main method.
+	 * 
+	 * System.out.println
+	 * 
+	 */
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Java");
+
+	}
+
+}
